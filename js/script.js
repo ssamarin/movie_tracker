5f71d11484f713241fbcec199cab2905eb5a1cd1
ssamarin <semenssamarin@mail.ru>
2022-08-12 19:37:55 +0900

    optimization and adding classification

--- js/script.js
+++ js/script.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--; 
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+    console.log("Вы классный зритель");
+} else if (personalMovieDB >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("ERROR");
+}
+
 
 console.log(personalMovieDB);
