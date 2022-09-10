6f57f6381f9fdd9a688a2c738f65ada3f4afc7ff
ssamarin <semenssamarin@mail.ru>
2022-09-10 19:39:48 +1000

    adjustment

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ const personalMovieDB = {
 
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', '');
-    const b = prompt('На сколько оцените его?', '');
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
@@ -36,5 +36,7 @@ if (personalMovieDB.count < 10) {
 
 
 console.log(personalMovieDB);
+
+
 
 
--- js/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-for (let i = 20; i => 10;) {
-    console.log (i)
-    i--;
-}
