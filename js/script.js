b78e9ae10ae88230db0b526e4ea9d98cea249904
ssamarin <semenssamarin@mail.ru>
2022-09-10 19:46:16 +1000

    fixed prompts

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const personalMovieDB = {
 
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+          b = +prompt('На сколько оцените его?', '');
 
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
